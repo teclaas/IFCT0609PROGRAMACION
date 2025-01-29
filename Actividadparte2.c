de68f58644040df14ad5e2cb8cc5af76de008ccd
Teclaas <naccga@outlook.com>
2025-01-29 10:03:13 +0100

    actividade 6 , 9 Y 10

---
+++ Actividadparte2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float sueldo;
+
+    // Solicitar al usuario que ingrese su sueldo
+    printf("Ingrese su sueldo: ");
+    scanf("%f", &sueldo);
+
+    // Verificar si el sueldo supera los 3000 pesos
+    if (sueldo > 3000) {
+        printf("Debe abonar impuestos.\n");
+    } else {
+        printf("No debe abonar impuestos.\n");
+    }
+
+    return 0;
+}
