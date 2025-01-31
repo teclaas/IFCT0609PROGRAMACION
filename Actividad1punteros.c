c8acd33eec1c44683de87733632572c8e08f8cf4
Teclaas <naccga@outlook.com>
2025-01-31 11:39:34 +0100

    actividad1punteros.c

--- Actividad1punteros.c
+++ Actividad1punteros.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // Inicializar el arreglo original
+    // Inicializar el arreglo original//
     int arregloOriginal[] = {1, 2, 3, 4, 5};
     int longitud = sizeof(arregloOriginal) / sizeof(arregloOriginal[0]);
 
-    // Crear un segundo arreglo para copiar los valores
+    // Crear un segundo arreglo para copiar los valores//
     int arregloCopiado[longitud];
 
-    // Copiar el contenido usando un bucle
+    // Copiar el contenido usando un bucle//
     for (int i = 0; i < longitud; i++) {
         arregloCopiado[i] = arregloOriginal[i];
     }
 
-    // Mostrar los valores de ambos arreglos
+    // Mostrar los valores de ambos arreglos//
     printf("Arreglo Original: ");
     for (int i = 0; i < longitud; i++) {
         printf("%d ", arregloOriginal[i]);
