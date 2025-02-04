33acfa9e95cce88db90fe43a72476f21ac515330
Teclaas <naccga@outlook.com>
2025-02-04 10:47:57 +0100

    actividad10extra.c

--- Actividadescondicionantes.c/actividad10extra.c
+++ Actividadescondicionantes.c/actividad10extra.c
@@ -3,16 +3,16 @@
 int main() {
     char letra;
 
-    // Solicitar una letra
+    // Solicitar una letra//
     printf("Introduce una letra: ");
     scanf(" %c", &letra);
 
-    // Convertir la letra a minúscula para simplificar la comparación
+    // Convertir la letra a minúscula para simplificar la comparación//
     if (letra >= 'A' && letra <= 'Z') {
         letra = letra + ('a' - 'A');
     }
 
-    // Detectar si es una vocal
+    // Detectar si es una vocal//
     if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
         printf("La letra '%c' es una vocal.\n", letra);
     } else {
