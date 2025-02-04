60a6aff594330ebe5b92cf80ff309a51c8044058
Teclaas <naccga@outlook.com>
2025-02-04 10:11:31 +0100

    actividad8.c

--- Actividadescondicionantes.c/Actividad8extra.c
+++ Actividadescondicionantes.c/Actividad8extra.c
@@ -3,11 +3,11 @@
 int main() {
     int numero;
 
-    // Solicitar un número
+    // Solicitar un número//
     printf("Introduce un número: ");
     scanf("%d", &numero);
 
-    // Determinar si el número es positivo o negativo
+    // Determinar si el número es positivo o negativo//
     if (numero > 0) {
         printf("El número %d es positivo.\n", numero);
     } else if (numero < 0) {
