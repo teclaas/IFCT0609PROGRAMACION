81971d138164271eaa950a08dbabe319e893a028
Teclaas <naccga@outlook.com>
2025-02-04 09:44:47 +0100

    actividad5extra.c

---
+++ Actividadescondicionantes.c/actividad5extra.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, temp;
+
+    // Solicitar los tres números//
+    printf("Introduce el primer número: ");
+    scanf("%d", &num1);
+    printf("Introduce el segundo número: ");
+    scanf("%d", &num2);
+    printf("Introduce el tercer número: ");
+    scanf("%d", &num3);
+
+    // Ordenar los números de menor a mayor//
+    if (num1 > num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if (num1 > num3) {
+        temp = num1;
+        num1 = num3;
+        num3 = temp;
+    }
+    if (num2 > num3) {
+        temp = num2;
+        num2 = num3;
+        num3 = temp;
+    }
+
+    // Mostrar los números ordenados//
+    printf("Los números ordenados de menor a mayor son: %d, %d, %d\n", num1, num2, num3);
+
+    return 0;
+}
